45763fdb787cf6bb8859574cc18093464934c22d
Michael <you@example.com>
2026-09-14 20:28:12 +0300

    initial commit6

--- src/IData.ts
+++ src/IData.ts
@@ -6,3 +6,20 @@ interface Makhdoom {
   success_rate: number;
   total_points: number;
 }
+interface Racer {
+  Name_of_the_person: any;
+  family_and_class: any;
+  points: number;
+}
+interface Question {
+  QuestionBody: any;
+  Choices: Choice;
+  CorrectChoice: any;
+  ref: any;
+}
+interface Choice {
+  A: any;
+  B: any;
+  C: any;
+  D: any;
+}
